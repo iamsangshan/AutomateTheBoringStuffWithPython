5a6bbb0af76abd7c83c59f7829ce9d8b599a6218
iamsangshan <58853757+iamsangshan@users.noreply.github.com>
2020-06-29 23:12:09 +0530

    OOP Python

---
+++ part-3/oop.py
@@ -0,0 +1,16 @@
+##### Object oriented Programming Python #####
+
+class Robot:
+    name;
+    master;
+    def Robot(name, master):
+        
+    def sayHi():
+        print("Hi, Good morning! I'm {this.name}")
+
+    def sayBye():
+        print("Good bye, {master}!")
+
+x1 = Robot("Britto", "Sangeeta")
+
+x1.sayHi()
